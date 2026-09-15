7e9c52c79bb2c8a9e182d6c2dc602552bd53925b
almickaeleleakim-create <al.mickael.eleakim@impatech.edu.br>
2026-09-15 16:45:16 -0300

    Atividade da 1ª aula

---
+++ entregas_3492/aula01/01_3492/mytools-main/main.py
@@ -0,0 +1,9 @@
+import MYTOOLS as t
+
+print("Teste de constantes: ")
+print("100 casas decimais de π:", t.PI_INT)
+print("100 casas decimais de e:", t.E_INT)
+
+print("\nTeste de funções: ")
+print("pi_real(5):", t.pi_real(5)) 
+print("e_real(4):", t.e_real(4))  
